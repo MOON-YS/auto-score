4b298ae47f6669bd0e3fd8928fb74cd203cd4b45
YUNSIK <mys990422@gmail.com>
2023-08-01 16:09:09 +0900

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import KEY
+import openai
 
-print(KEY.OPEN_API_KEY)
+openai.api_key = KEY.OPEN_API_KEY
+
+messages = []
+while True:
+    content = input("User: ")
+    messages.append({'role' : 'user','content':content})
+    
+    completion = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+    chat_response = completion.choices[0].message.content
+    print(f'ChatGPT: {chat_response}')
+    messages.append({'role' : 'user','content':chat_response})
