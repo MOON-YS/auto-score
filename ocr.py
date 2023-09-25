d93b181d4965481fb05bc08f5db841a9ec451e51
YUNSIK <mys990422@gmail.com>
2023-09-25 19:24:02 +0900

    23.09.25 객관식 자동채점

--- ocr.py
+++ ocr.py
@@ -1,5 +1,6 @@
 import os
 import KEY
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = KEY.GOOGLE_API_KEY
 
 def detect_document(path):
@@ -24,6 +25,30 @@ def detect_document(path):
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
+
+def detect_number_from_img(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    print(response)
+   
+    return response.full_text_annotation.text
+
+    
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
 
 if __name__ == '__main__':
     img ='sample.jpg'   
